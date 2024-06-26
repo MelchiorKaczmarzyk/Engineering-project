29cad48ab558681f4a2fc60cc889efb80ad5a48b
MelchioKaczmarzyk <melchior.kaczmarzyk@gmail.com>
2024-06-26 13:57:59 +0200

    Committing backend

--- Frontend/src/app/gm/GmForSession.ts
+++
@@ -1,4 +0,0 @@
-export class GmForSession
-{
-    name : string = ""
-}
--- Frontend/src/app/session/ISessionPost.ts
+++
@@ -1,9 +0,0 @@
-import { GmForSession } from "../gm/GmForSession"
-
-export default interface ISessionPost {
-    system : string
-    title : string
-    tags : string
-    description : string
-    maxNumberOfPlayers : number
-}
